55eac8a1f0bf04b69727e46823feb60656177249
Sara Feraca <sara.feraca@duke.edu>
2019-07-24 21:12:52 -0700

    red and baskerville

--- ui/src/Count.js
+++ ui/src/Count.js
@@ -11,8 +11,29 @@ class Count extends React.Component {
     return (
       <div className="Count">
         <header>
-          <h1 >Number of Sexual Assaults Reported.</h1>
-          <h1> {count}</h1>
+          <div className="wrapper">
+            <div className="inline">
+              <p style={{display:'inline-block', padding: '4px'}}className="red"> 7000</p>
+              <p style={{display:'inline-block',padding: '4px'}}> undergraduates currently enrolled at duke</p>
+            </div>
+            <div className="inline">
+              <p style={{display:'inline-block',padding: '4px'}}className="red"> 0 </p>
+              <p style={{display:'inline-block',padding: '4px'}}>days since school started</p>
+            </div>
+            <div className="inline">
+              <p style={{display:'inline-block',padding: '4px'}}className="red">0 </p>
+              <p style={{display:'inline-block',padding: '4px'}}>students assaulted this academic year</p>
+            </div>
+            <div className="inline">
+              <p style={{display:'inline-block',padding: '4px'}}className="red">0 </p>
+              <p style={{display:'inline-block',padding: '4px'}}> assaults officially reported this academic year</p>
+            </div>
+          </div>
+          <div className="number">
+            <p className="bignumber"> {count}</p>
+            <h1 className="subtype">Number of Sexual Assaults Reported.</h1>
+          
+          </div>
         </header>
 
       </div>
